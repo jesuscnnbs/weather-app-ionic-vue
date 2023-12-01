26992e272ebf07e7fc8f8e03a19b16bffc8d7b78
Jesús García Ortuño <jesuscnnbs@gmail.com>
2023-12-01 11:04:34 +0100

    create ionic app

---
+++ src/components/Topbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ $route.params.id }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+</template>
+
+<script setup lang="ts">
+import { IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -1,21 +1,7 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
-        </ion-buttons>
-        <ion-title>{{ $route.params.id }}</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
+    <Topbar />
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">{{ $route.params.id }}</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <div id="container">
         <strong class="capitalize">{{ $route.params.id }}</strong>
         <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
@@ -25,7 +11,13 @@
 </template>
 
 <script setup lang="ts">
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { Topbar } from '../components';
+
+defineComponent({
+  components: { Topbar, IonContent, IonPage },
+});
 </script>
 
 <style scoped>
